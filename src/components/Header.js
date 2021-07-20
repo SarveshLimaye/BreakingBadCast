2a77d425a7e13349f6ad4e2c84b4351dfa7edeb4
SarveshLimaye <74766567+SarveshLimaye@users.noreply.github.com>
2021-07-20 14:19:31 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "../components/Header.css"
 const header = () => {
     return (
         <div className="image-div">
-            <img className="image" src="https://i.pinimg.com/originals/e5/0f/52/e50f52864b4a50cce31829910b6a58af.png"/>
+            <img className="image" alt="added alt prop" src="https://i.pinimg.com/originals/e5/0f/52/e50f52864b4a50cce31829910b6a58af.png"/>
         </div>
     )
 
